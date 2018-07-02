d4c226b6e136bd800be661843574217c50b39d32
Julia Lidstrom <juliag@young-america.com>
2018-07-02 15:33:57 -0500

    Rendered OSL page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import Registration from "./components/Registration";
+// import Registration from "./components/Registration";
+import OSL from "./components/OSL";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Registration />
+        {/* <Registration /> */}
+        <OSL />
       </div>
     );
   }
